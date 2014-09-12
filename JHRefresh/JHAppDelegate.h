5eeac81aa61937974d400b7d2babe285ede9c348
Jiahai <Jiahai@jiahaideMacBook-Pro.local>
2014-09-12 09:34:01 +0800

    Initial Commit

---
+++ JHRefresh/JHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JHAppDelegate.h
+//  JHRefresh
+//
+//  Created by Jiahai on 14-9-12.
+//  Copyright (c) 2014年 Jiahai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JHRefresh/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JHRefresh
+//
+//  Created by Jiahai on 14-9-12.
+//  Copyright (c) 2014年 Jiahai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JHAppDelegate class]));
+    }
+}
